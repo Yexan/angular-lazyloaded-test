add01fb00ea770bae005eb0a61dc370ff573d15a
Yexan <contact.adrien.martinet@gmail.com>
2023-06-20 10:12:19 +0200

    tests

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'front',
-    redirectTo: '',
+    redirectTo: '/',
     loadChildren: () => import('./front/front.module').then(m => m.FrontModule)
   },
   {
